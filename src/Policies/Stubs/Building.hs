11eb2edf609b76e60c566887952a5d66bd3dc523
Ennio Visconti <ennio.visconti@gmail.com>
2018-12-27 14:48:49 +0900

    switched to reimscheid-ready parser version

--- src/Policies/Stubs/Building.hs
+++ src/Policies/Stubs/Building.hs
@@ -2,152 +2,166 @@ module Policies.Stubs.Building (sBuilding) where
 
 import           CityGML.Types
 
-sBuilding i = Building (sFeature i)  (Just $ Height "#m" 100) Nothing Nothing Nothing Nothing  l0f l0r l1s Nothing [] []
+sBuilding i =
+    Building
+        ( BldgData
+            (sObject i)
+            []
+            (BuildingInfo Nothing Nothing (Just $ Height "#m" 100) Nothing Nothing)
+            (BuildingModels l0f l0r l1s Nothing Nothing)
+            (BuildingIntersections Nothing Nothing)
+            []
+            []
+            []
+            Nothing
+        )
+
+sObject  i = CityObject (sFeature i) Nothing Nothing [] [] Nothing Nothing
 sFeature i = Feature (GML (Just i) [] Nothing) Nothing
 
 l0f = Just (FootPrint fp)
 l0r = Just (RoofEdge  re)
 l1s = Just (BldgLod1Solid (Solid cs []))
 
-fp = MultiSurface
+fp = MultiSurface (Feature (GML Nothing [] Nothing) Nothing)
     [
         Polygon (Feature (GML Nothing [] Nothing) Nothing)
-            (LinearRing
-              [  Coord "85010.059 447216.119 0.000"
-              ,  Coord "85065.266 447162.431 0.000"
-              ,  Coord "85069.605 447169.206 0.000"
-              ,  Coord "85067.298 447170.684 0.000"
-              ,  Coord "85069.019 447173.370 0.000"
-              ,  Coord "85068.943 447173.419 0.000"
-              ,  Coord "85070.556 447175.936 0.000"
-              ,  Coord "85017.737 447228.101 0.000"
-              ,  Coord "85010.059 447216.119 0.000"
+            (LinearRing (sFeature "RandomID")
+              [  Coord "85010.059 447216.119 0.000"  Nothing
+              ,  Coord "85065.266 447162.431 0.000"  Nothing
+              ,  Coord "85069.605 447169.206 0.000"  Nothing
+              ,  Coord "85067.298 447170.684 0.000"  Nothing
+              ,  Coord "85069.019 447173.370 0.000"  Nothing
+              ,  Coord "85068.943 447173.419 0.000"  Nothing
+              ,  Coord "85070.556 447175.936 0.000"  Nothing
+              ,  Coord "85017.737 447228.101 0.000"  Nothing
+              ,  Coord "85010.059 447216.119 0.000"  Nothing
               ]) []
     ]
 
-re = MultiSurface
+re = MultiSurface (Feature (GML Nothing [] Nothing) Nothing)
     [
         Polygon (Feature (GML Nothing [] Nothing) Nothing)
-            (LinearRing
-              [  Coord "85010.059 447216.119 70.000"
-              ,  Coord "85065.266 447162.431 70.000"
-              ,  Coord "85069.605 447169.206 70.000"
-              ,  Coord "85067.298 447170.684 70.000"
-              ,  Coord "85069.019 447173.370 70.000"
-              ,  Coord "85068.943 447173.419 70.000"
-              ,  Coord "85070.556 447175.936 70.000"
-              ,  Coord "85017.737 447228.101 70.000"
-              ,  Coord "85010.059 447216.119 70.000"
+            (LinearRing (sFeature "RandomID")
+              [  Coord "85010.059 447216.119 70.000" Nothing
+              ,  Coord "85065.266 447162.431 70.000" Nothing
+              ,  Coord "85069.605 447169.206 70.000" Nothing
+              ,  Coord "85067.298 447170.684 70.000" Nothing
+              ,  Coord "85069.019 447173.370 70.000" Nothing
+              ,  Coord "85068.943 447173.419 70.000" Nothing
+              ,  Coord "85070.556 447175.936 70.000" Nothing
+              ,  Coord "85017.737 447228.101 70.000" Nothing
+              ,  Coord "85010.059 447216.119 70.000" Nothing
               ]) []
     ]
 
 cs = CompositeSurface
     [
         Polygon (Feature (GML Nothing [] Nothing) Nothing)
-            (LinearRing
-              [  Coord "85017.737 447228.101 0.000"
-              ,  Coord "85070.556 447175.936 0.000"
-              ,  Coord "85068.943 447173.419 0.000"
-              ,  Coord "85069.019 447173.370 0.000"
-              ,  Coord "85067.298 447170.684 0.000"
-              ,  Coord "85069.605 447169.206 0.000"
-              ,  Coord "85065.266 447162.431 0.000"
-              ,  Coord "85010.059 447216.119 0.000"
-              ,  Coord "85017.737 447228.101 0.000"
+            (LinearRing (sFeature "RandomID")
+              [  Coord "85017.737 447228.101 0.000"  Nothing
+              ,  Coord "85070.556 447175.936 0.000"  Nothing
+              ,  Coord "85068.943 447173.419 0.000"  Nothing
+              ,  Coord "85069.019 447173.370 0.000"  Nothing
+              ,  Coord "85067.298 447170.684 0.000"  Nothing
+              ,  Coord "85069.605 447169.206 0.000"  Nothing
+              ,  Coord "85065.266 447162.431 0.000"  Nothing
+              ,  Coord "85010.059 447216.119 0.000"  Nothing
+              ,  Coord "85017.737 447228.101 0.000"  Nothing
               ]
             ) []
         ,
         Polygon (Feature (GML Nothing [] Nothing) Nothing)
-            (LinearRing
-              [  Coord "85010.059 447216.119 70.000"
-              ,  Coord "85065.266 447162.431 70.000"
-              ,  Coord "85069.605 447169.206 70.000"
-              ,  Coord "85067.298 447170.684 70.000"
-              ,  Coord "85069.019 447173.370 70.000"
-              ,  Coord "85068.943 447173.419 70.000"
-              ,  Coord "85070.556 447175.936 70.000"
-              ,  Coord "85017.737 447228.101 70.000"
-              ,  Coord "85010.059 447216.119 70.000"
+            (LinearRing (sFeature "RandomID")
+              [  Coord "85010.059 447216.119 70.000" Nothing
+              ,  Coord "85065.266 447162.431 70.000" Nothing
+              ,  Coord "85069.605 447169.206 70.000" Nothing
+              ,  Coord "85067.298 447170.684 70.000" Nothing
+              ,  Coord "85069.019 447173.370 70.000" Nothing
+              ,  Coord "85068.943 447173.419 70.000" Nothing
+              ,  Coord "85070.556 447175.936 70.000" Nothing
+              ,  Coord "85017.737 447228.101 70.000" Nothing
+              ,  Coord "85010.059 447216.119 70.000" Nothing
               ]
             ) []
         ,
         Polygon (Feature (GML Nothing [] Nothing) Nothing)
-            (LinearRing
-              [  Coord "85070.556 447175.936 0.000"
-              ,  Coord "85017.737 447228.101 0.000"
-              ,  Coord "85017.737 447228.101 70.000"
-              ,  Coord "85070.556 447175.936 70.000"
-              ,  Coord "85070.556 447175.936 0.000"
+            (LinearRing (sFeature "RandomID")
+              [  Coord "85070.556 447175.936 0.000"  Nothing
+              ,  Coord "85017.737 447228.101 0.000"  Nothing
+              ,  Coord "85017.737 447228.101 70.000" Nothing
+              ,  Coord "85070.556 447175.936 70.000" Nothing
+              ,  Coord "85070.556 447175.936 0.000"  Nothing
               ]
             ) []
         ,
         Polygon (Feature (GML Nothing [] Nothing) Nothing)
-            (LinearRing
-              [  Coord "85068.943 447173.419 0.000"
-              ,  Coord "85070.556 447175.936 0.000"
-              ,  Coord "85070.556 447175.936 70.000"
-              ,  Coord "85068.943 447173.419 70.000"
-              ,  Coord "85068.943 447173.419 0.000"
+            (LinearRing (sFeature "RandomID")
+              [  Coord "85068.943 447173.419 0.000"  Nothing
+              ,  Coord "85070.556 447175.936 0.000"  Nothing
+              ,  Coord "85070.556 447175.936 70.000" Nothing
+              ,  Coord "85068.943 447173.419 70.000" Nothing
+              ,  Coord "85068.943 447173.419 0.000"  Nothing
               ]
             ) []
         ,
         Polygon (Feature (GML Nothing [] Nothing) Nothing)
-            (LinearRing
-              [  Coord "85069.019 447173.370 0.000"
-              ,  Coord "85068.943 447173.419 0.000"
-              ,  Coord "85068.943 447173.419 70.000"
-              ,  Coord "85069.019 447173.370 70.000"
-              ,  Coord "85069.019 447173.370 0.000"
+            (LinearRing (sFeature "RandomID")
+              [  Coord "85069.019 447173.370 0.000"  Nothing
+              ,  Coord "85068.943 447173.419 0.000"  Nothing
+              ,  Coord "85068.943 447173.419 70.000" Nothing
+              ,  Coord "85069.019 447173.370 70.000" Nothing
+              ,  Coord "85069.019 447173.370 0.000"  Nothing
               ]
             ) []
         ,
         Polygon (Feature (GML Nothing [] Nothing) Nothing)
-            (LinearRing
-              [  Coord "85067.298 447170.684 0.000"
-              ,  Coord "85069.019 447173.370 0.000"
-              ,  Coord "85069.019 447173.370 70.000"
-              ,  Coord "85067.298 447170.684 70.000"
-              ,  Coord "85067.298 447170.684 0.000"
+            (LinearRing (sFeature "RandomID")
+              [  Coord "85067.298 447170.684 0.000"  Nothing
+              ,  Coord "85069.019 447173.370 0.000"  Nothing
+              ,  Coord "85069.019 447173.370 70.000" Nothing
+              ,  Coord "85067.298 447170.684 70.000" Nothing
+              ,  Coord "85067.298 447170.684 0.000"  Nothing
               ]
             ) []
         ,
         Polygon (Feature (GML Nothing [] Nothing) Nothing)
-            (LinearRing
-              [  Coord "85069.605 447169.206 0.000"
-              ,  Coord "85067.298 447170.684 0.000"
-              ,  Coord "85067.298 447170.684 70.000"
-              ,  Coord "85069.605 447169.206 70.000"
-              ,  Coord "85069.605 447169.206 0.000"
+            (LinearRing (sFeature "RandomID")
+              [  Coord "85069.605 447169.206 0.000"  Nothing
+              ,  Coord "85067.298 447170.684 0.000"  Nothing
+              ,  Coord "85067.298 447170.684 70.000" Nothing
+              ,  Coord "85069.605 447169.206 70.000" Nothing
+              ,  Coord "85069.605 447169.206 0.000"  Nothing
               ]
             ) []
         ,
         Polygon (Feature (GML Nothing [] Nothing) Nothing)
-            (LinearRing
-              [  Coord "85065.266 447162.431 0.000"
-              ,  Coord "85069.605 447169.206 0.000"
-              ,  Coord "85069.605 447169.206 70.000"
-              ,  Coord "85065.266 447162.431 70.000"
-              ,  Coord "85065.266 447162.431 0.000"
+            (LinearRing (sFeature "RandomID")
+              [  Coord "85065.266 447162.431 0.000"  Nothing
+              ,  Coord "85069.605 447169.206 0.000"  Nothing
+              ,  Coord "85069.605 447169.206 70.000" Nothing
+              ,  Coord "85065.266 447162.431 70.000" Nothing
+              ,  Coord "85065.266 447162.431 0.000"  Nothing
               ]
             ) []
         ,
         Polygon (Feature (GML Nothing [] Nothing) Nothing)
-            (LinearRing
-              [  Coord "85010.059 447216.119 0.000"
-              ,  Coord "85065.266 447162.431 0.000"
-              ,  Coord "85065.266 447162.431 70.000"
-              ,  Coord "85010.059 447216.119 70.000"
-              ,  Coord "85010.059 447216.119 0.000"
+            (LinearRing (sFeature "RandomID")
+              [  Coord "85010.059 447216.119 0.000"  Nothing
+              ,  Coord "85065.266 447162.431 0.000"  Nothing
+              ,  Coord "85065.266 447162.431 70.000" Nothing
+              ,  Coord "85010.059 447216.119 70.000" Nothing
+              ,  Coord "85010.059 447216.119 0.000"  Nothing
               ]
             ) []
         ,
         Polygon (Feature (GML Nothing [] Nothing) Nothing)
-            (LinearRing
-              [  Coord "85017.737 447228.101 0.000"
-              ,  Coord "85010.059 447216.119 0.000"
-              ,  Coord "85010.059 447216.119 70.000"
-              ,  Coord "85017.737 447228.101 70.000"
-              ,  Coord "85017.737 447228.101 0.000"
+            (LinearRing (sFeature "RandomID")
+              [  Coord "85017.737 447228.101 0.000"  Nothing
+              ,  Coord "85010.059 447216.119 0.000"  Nothing
+              ,  Coord "85010.059 447216.119 70.000" Nothing
+              ,  Coord "85017.737 447228.101 70.000" Nothing
+              ,  Coord "85017.737 447228.101 0.000"  Nothing
               ]
             ) []
     ]
