047d20b35fe329cb4b39db2eb9aaf031e8abb7e5
Ennio Visconti <ennio.visconti@gmail.com>
2019-01-25 18:19:49 +0900

    CLI base impl and update of citygml4hs.

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,38 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 import           Lib
 
+-- main :: IO ()
+-- main = wip
+
+import           Data.Maybe
+import           Data.Text
+import           Data.Version   (showVersion)
+import           Paths_topocity (version)
+import           Turtle
+
+mainSubroutine :: IO ()
+mainSubroutine = echo $ unsafeTextToLine $ pack $ "Topocity alpha v." ++ (showVersion version)
+
+parseMain :: Parser (IO ())
+parseMain = pure mainSubroutine
+
+parser :: Parser (IO ())
+parser = parseMain
+
 main :: IO ()
-main = wip
+main = do
+    cmd <- options "topocity: a prototype framework for BX between CityGML and CPSps" parser
+    cmd
+
+{-}
+printText :: (Maybe Int, Text) -> IO()
+printText (Nothing, text)  = echo text
+printText ((Just i), text) = replicateM_ i (echo text)
+
+printArgs :: Parser (Maybe Int, Text)
+printArgs = (,) <$> optional (optInt "times" 'n' "Number of times")
+                <*> (argText "text" "Text to print")
+-}
