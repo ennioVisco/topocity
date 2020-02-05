9e7a4d0db560691cb029bf4e7c9e4bfe65f8a6ca
Ennio Visconti <ennio.visconti@gmail.com>
2020-02-05 12:42:01 +0100

    Update to citygml4hs@a9ddbf5

--- src/Policies/Stubs/Building.hs
+++ src/Policies/Stubs/Building.hs
@@ -17,15 +17,16 @@ sBuilding i =
 
 ssObject  i = CityObject (sFeature i) Nothing Nothing [] [] Nothing Nothing []
 sFeature i = Feature (GML (Just i) [] Nothing) Nothing
+sGeometry i = AbstractGeometry (GML (Just i) [] Nothing) Nothing
 
 l0f = Just (FootPrint fp)
 l0r = Just (RoofEdge  re)
-l1s = Just (BldgLod1Solid (Solid cs []))
+l1s = Just (BldgLod1Solid (Solid (sGeometry "RandomSolid") cs []))
 
-fp = MultiSurface (Feature (GML Nothing [] Nothing) Nothing)
+fp = MultiSurface (AbstractGeometry (GML Nothing [] Nothing) Nothing)
     [
-        Polygon (Feature (GML Nothing [] Nothing) Nothing)
-            (LinearRing (sFeature "RandomID")
+        Polygon (AbstractGeometry (GML Nothing [] Nothing) Nothing)
+            (LinearRing (sGeometry "RandomID")
               [  Coord "85010.059 447216.119 0.000"  Nothing
               ,  Coord "85065.266 447162.431 0.000"  Nothing
               ,  Coord "85069.605 447169.206 0.000"  Nothing
@@ -38,10 +39,10 @@ fp = MultiSurface (Feature (GML Nothing [] Nothing) Nothing)
               ]) []
     ]
 
-re = MultiSurface (Feature (GML Nothing [] Nothing) Nothing)
+re = MultiSurface (AbstractGeometry (GML Nothing [] Nothing) Nothing)
     [
-        Polygon (Feature (GML Nothing [] Nothing) Nothing)
-            (LinearRing (sFeature "RandomID")
+        Polygon (AbstractGeometry (GML Nothing [] Nothing) Nothing)
+            (LinearRing (sGeometry "RandomID")
               [  Coord "85010.059 447216.119 70.000" Nothing
               ,  Coord "85065.266 447162.431 70.000" Nothing
               ,  Coord "85069.605 447169.206 70.000" Nothing
@@ -54,10 +55,10 @@ re = MultiSurface (Feature (GML Nothing [] Nothing) Nothing)
               ]) []
     ]
 
-cs = CompositeSurface (sFeature "randomSurf")
+cs = CompositeSurface (sGeometry "randomSurf")
     [
-        Polygon (Feature (GML Nothing [] Nothing) Nothing)
-            (LinearRing (sFeature "RandomID")
+        Polygon (AbstractGeometry (GML Nothing [] Nothing) Nothing)
+            (LinearRing (sGeometry "RandomID")
               [  Coord "85017.737 447228.101 0.000"  Nothing
               ,  Coord "85070.556 447175.936 0.000"  Nothing
               ,  Coord "85068.943 447173.419 0.000"  Nothing
@@ -70,8 +71,8 @@ cs = CompositeSurface (sFeature "randomSurf")
               ]
             ) []
         ,
-        Polygon (Feature (GML Nothing [] Nothing) Nothing)
-            (LinearRing (sFeature "RandomID")
+        Polygon (AbstractGeometry (GML Nothing [] Nothing) Nothing)
+            (LinearRing (sGeometry "RandomID")
               [  Coord "85010.059 447216.119 70.000" Nothing
               ,  Coord "85065.266 447162.431 70.000" Nothing
               ,  Coord "85069.605 447169.206 70.000" Nothing
@@ -84,8 +85,8 @@ cs = CompositeSurface (sFeature "randomSurf")
               ]
             ) []
         ,
-        Polygon (Feature (GML Nothing [] Nothing) Nothing)
-            (LinearRing (sFeature "RandomID")
+        Polygon (AbstractGeometry (GML Nothing [] Nothing) Nothing)
+            (LinearRing (sGeometry "RandomID")
               [  Coord "85070.556 447175.936 0.000"  Nothing
               ,  Coord "85017.737 447228.101 0.000"  Nothing
               ,  Coord "85017.737 447228.101 70.000" Nothing
@@ -94,8 +95,8 @@ cs = CompositeSurface (sFeature "randomSurf")
               ]
             ) []
         ,
-        Polygon (Feature (GML Nothing [] Nothing) Nothing)
-            (LinearRing (sFeature "RandomID")
+        Polygon (AbstractGeometry (GML Nothing [] Nothing) Nothing)
+            (LinearRing (sGeometry "RandomID")
               [  Coord "85068.943 447173.419 0.000"  Nothing
               ,  Coord "85070.556 447175.936 0.000"  Nothing
               ,  Coord "85070.556 447175.936 70.000" Nothing
@@ -104,8 +105,8 @@ cs = CompositeSurface (sFeature "randomSurf")
               ]
             ) []
         ,
-        Polygon (Feature (GML Nothing [] Nothing) Nothing)
-            (LinearRing (sFeature "RandomID")
+        Polygon (AbstractGeometry (GML Nothing [] Nothing) Nothing)
+            (LinearRing (sGeometry "RandomID")
               [  Coord "85069.019 447173.370 0.000"  Nothing
               ,  Coord "85068.943 447173.419 0.000"  Nothing
               ,  Coord "85068.943 447173.419 70.000" Nothing
@@ -114,8 +115,8 @@ cs = CompositeSurface (sFeature "randomSurf")
               ]
             ) []
         ,
-        Polygon (Feature (GML Nothing [] Nothing) Nothing)
-            (LinearRing (sFeature "RandomID")
+        Polygon (AbstractGeometry (GML Nothing [] Nothing) Nothing)
+            (LinearRing (sGeometry "RandomID")
               [  Coord "85067.298 447170.684 0.000"  Nothing
               ,  Coord "85069.019 447173.370 0.000"  Nothing
               ,  Coord "85069.019 447173.370 70.000" Nothing
@@ -124,8 +125,8 @@ cs = CompositeSurface (sFeature "randomSurf")
               ]
             ) []
         ,
-        Polygon (Feature (GML Nothing [] Nothing) Nothing)
-            (LinearRing (sFeature "RandomID")
+        Polygon (AbstractGeometry (GML Nothing [] Nothing) Nothing)
+            (LinearRing (sGeometry "RandomID")
               [  Coord "85069.605 447169.206 0.000"  Nothing
               ,  Coord "85067.298 447170.684 0.000"  Nothing
               ,  Coord "85067.298 447170.684 70.000" Nothing
@@ -134,8 +135,8 @@ cs = CompositeSurface (sFeature "randomSurf")
               ]
             ) []
         ,
-        Polygon (Feature (GML Nothing [] Nothing) Nothing)
-            (LinearRing (sFeature "RandomID")
+        Polygon (AbstractGeometry (GML Nothing [] Nothing) Nothing)
+            (LinearRing (sGeometry "RandomID")
               [  Coord "85065.266 447162.431 0.000"  Nothing
               ,  Coord "85069.605 447169.206 0.000"  Nothing
               ,  Coord "85069.605 447169.206 70.000" Nothing
@@ -144,8 +145,8 @@ cs = CompositeSurface (sFeature "randomSurf")
               ]
             ) []
         ,
-        Polygon (Feature (GML Nothing [] Nothing) Nothing)
-            (LinearRing (sFeature "RandomID")
+        Polygon (AbstractGeometry (GML Nothing [] Nothing) Nothing)
+            (LinearRing (sGeometry "RandomID")
               [  Coord "85010.059 447216.119 0.000"  Nothing
               ,  Coord "85065.266 447162.431 0.000"  Nothing
               ,  Coord "85065.266 447162.431 70.000" Nothing
@@ -154,8 +155,8 @@ cs = CompositeSurface (sFeature "randomSurf")
               ]
             ) []
         ,
-        Polygon (Feature (GML Nothing [] Nothing) Nothing)
-            (LinearRing (sFeature "RandomID")
+        Polygon (AbstractGeometry (GML Nothing [] Nothing) Nothing)
+            (LinearRing (sGeometry "RandomID")
               [  Coord "85017.737 447228.101 0.000"  Nothing
               ,  Coord "85010.059 447216.119 0.000"  Nothing
               ,  Coord "85010.059 447216.119 70.000" Nothing
